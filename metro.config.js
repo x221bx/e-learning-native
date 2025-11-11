4b55d4e93b2e944bcf4902d75ef95be5a2a2ee15
ahmad <ahmad1zain10@gmail.com>
2025-11-11 13:38:39 +0200

    Switch Expo web builds to Metro

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,17 @@
-// Standard Expo Metro configuration to ensure the CLI resolves Metro correctly
+// Expo Metro configuration extended with browser polyfills for Node modules
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver = {
+  ...(config.resolver || {}),
+  extraNodeModules: {
+    ...((config.resolver && config.resolver.extraNodeModules) || {}),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer/'),
+    process: require.resolve('process/browser'),
+  },
+};
 
 module.exports = config;
